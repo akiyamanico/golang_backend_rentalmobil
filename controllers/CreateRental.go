58803443647c722c9148f109a09daac8fcb7fca1
Fi Zilalil Huda <76860149+akiyamanico@users.noreply.github.com>
2024-02-26 13:56:14 +0700

    aaaa

--- controllers/CreateRental.go
+++ controllers/CreateRental.go
@@ -3,52 +3,44 @@ package controllers
 import (
 	"backend_test/models"
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func CreateRental(c *gin.Context) {
-
 	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/rentalmobil")
 	if err != nil {
 		log.Fatal(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
 		return
 	}
-
 	var request models.CreateRental
-	idStr := strconv.Itoa(int(request.ID))
-	fmt.Println("ini id", request.ID)
-
 	form, err := c.MultipartForm()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	fmt.Println("ini form", form.Value)
 
+	idValues := form.Value["id"]
+	idValue := idValues[0]
 	files := form.File["dokumenpinjaman"]
 	if len(files) == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "No file uploaded"})
 		return
 	}
-	fmt.Println(request.ID)
+
 	file := files[0]
 	query := "SELECT status, namamobil FROM status_list_rentals WHERE id = ?"
-	row := db.QueryRow(query, 2)
+	row := db.QueryRow(query, idValue)
 
 	imageName := file.Filename
 	var status models.CreateRental
 	username := c.PostForm("username")
 	request.Dokumenpinjaman = imageName
 	request.Username = username
-
-	fmt.Println("ini id", idStr)
 	err = row.Scan(&status.Status, &status.Pinjaman)
 	if err != nil {
 		log.Println(err)
